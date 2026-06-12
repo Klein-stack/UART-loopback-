94eb798c5ff41c071ea707b14daf1a1d30762c7d
Klein-stack <sandeepsnathan08@gmail.com>
2026-06-12 22:35:59 +0530

    updated main.c

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -2,11 +2,13 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
+#include <string.h>
 
 
 #define buffer_size 1024
-#define TX_PIN 
-#define RX_PIN 
+#define TX_PIN 21
+#define RX_PIN 20
+#define TASK_STACK_SIZE 4096
 
 static void loopback(void *arg){
     QueueHandle_t uart_queue;
@@ -22,8 +24,21 @@ static void loopback(void *arg){
     };
     uart_param_config(UART_NUM_1, &config);
     uart_set_pin(UART_NUM_1, TX_PIN, RX_PIN, -1,-1 );
+
+    char send_data[] = "Miles to go before I sleep!";
+    uart_flush(UART_NUM_1);
+    uart_write_bytes(UART_NUM_1, send_data, strlen(send_data));
+    
+    uint8_t data[128];
+    int length = 0;
+    uart_get_buffered_data_len(UART_NUM_1, (size_t*)&length);
+    length = uart_read_bytes(UART_NUM_1, data, length, 100);
+    printf("Received data: %s",data);
+
+
+
 }
-
+    
 void app_main(void){
-
+    xTaskCreate(loopback, "Loopback_task", TASK_STACK_SIZE, NULL, 1, NULL);
 }
